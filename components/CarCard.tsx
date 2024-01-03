47bb397ce76845eccb7b8aed0baa81b0f1c4349b
VastoLorde <ksenbonsakura@gmail.com>
2024-01-03 10:05:49 +0100

    FEAT: Integrate Car images API

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 import { CarProps } from "@/types";
 import { CarDetails, CustomButton } from ".";
-import { calculateCarRent } from "@/utils";
+import { calculateCarRent, generateCarImageUrl } from "@/utils";
 
 interface CarCardProps {
     car: CarProps;
@@ -37,7 +37,7 @@ const CarCard = ({ car }: CarCardProps) => {
         </p>
 
         <div className="relative w-full h-40 my-3 object-contain">
-            <Image src="/hero.png" alt="car model" fill priority className="object-contain" />
+            <Image src={generateCarImageUrl(car)} alt="car model" fill priority className="object-contain" />
         </div>
 
         <div className="relative flex w-full mt-2">
--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    /* The `images` configuration in the `nextConfig` object is specifying the domains from which
+    Next.js should load images. In this case, it is specifying that images should be loaded from the
+    domain `cdn.imagin.studio`. This allows Next.js to optimize and handle the loading of images
+    from that domain. */
+    images: {
+        domains: ['cdn.imagin.studio']
+    }
+}
 
 module.exports = nextConfig
